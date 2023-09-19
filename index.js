a7faa1ad18be988e4530eea752776501cc691f07
mui <hiro82585@gmail.com>
2023-09-19 21:21:08 +0900

    117行目にバグがあって正常に動かない

--- index.js
+++ index.js
@@ -63,15 +63,14 @@ const getLikesMedia = async (userId) => {
 }
 
 const checkSQL = async (data, mediaInfo) => {
-  //resultがすでにSQliteに載ってるかを確認
+  // https://qiita.com/zaburo/items/a155cbc02832b501a8dd
+  // node.jsでsqlite3を利用する
+  // resultがすでにSQliteに載ってるかを確認
   let sql
-  db.serialize(() => {
-    sql = "SELECT COUNT(*) FROM tweet WHERE file_name = ? LIMIT 1;"
-    db.get(sql, [mediaInfo.file_name], (err, row) => { // ツイートIDで検索
-      if (err) return console.log("sql error\n" + err.message);
-      if (row['COUNT(*)'] !== 0) return ;
-    })
-
+  sql = "SELECT COUNT(*) FROM tweet WHERE file_name = ? LIMIT 1;"
+  db.get(sql, [mediaInfo.file_name], (err, row) => { // ツイートIDで検索
+    if (err) return console.log("sql error\n" + err.message);
+    if (row['COUNT(*)'] !== 0) return;
     //新規のデータであった場合resultをSQliteに格納
     sql = "INSERT INTO tweet (author_screenname,author_name, author_id, tweet_id, media_url, file_name) VALUES(?,?,?,?,?,?);"
     const param = [
@@ -110,12 +109,15 @@ const getAllMedia = async () => {
   for await (const user of config["UserList"]) {
     const tweets = await getLikesMedia(user.userId); // メディアツイート情報取得
     for await (const tweet of tweets) { // ツイート個々に対して処理
-      const mediaInfo = await getMediaInfo(tweet);
+      // 保存するのに必要なurlとファイル名を抜き出し
+      const mediaInfoArray = await getMediaInfo(tweet);
       // sqlの中のデータと照合して、存在しなかったら保存する
-      for (const media of mediaInfo) {
-        const existInSQL = await checkSQL(tweet, media)
+      for await (const mediaInfo of mediaInfoArray) {
+        const existInSQL = await checkSQL(tweet, mediaInfo)
+        console.log("existInSQL:", existInSQL) // ->`existInSQL: undefined`になってしまう
+        console.log(mediaInfo)
         if (existInSQL) {
-          allPictures.push(media)
+          allPictures.push(mediaInfo)
         }
       }
     }
