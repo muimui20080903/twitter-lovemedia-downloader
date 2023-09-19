0d59271123a38869f097847740a83348b26f0d3c
mui <hiro82585@gmail.com>
2023-09-19 20:15:25 +0900

    動画も動作するように変更

--- index.js
+++ index.js
@@ -6,13 +6,14 @@ const config = JSON.parse(await readFile("./config.json"))
 import { TwitterOpenApi } from "twitter-openapi-typescript"
 import fetch from "node-fetch"
 const api = new TwitterOpenApi({ fetchApi: fetch })
+const client = await api.getClientFromCookies(config.ct0, config.authToken)
 import sqlite3 from "sqlite3"
 const db = new sqlite3.Database(config.dbdir)
 
 const getMediaInfo = async (data) => {
-  const tweet = data.tweet.legacy
   // メディア情報の初期化
   const mediaInfo = []
+  const tweet = data.tweet.legacy
   const hasVideo = tweet.extendedEntities && tweet.extendedEntities.media && tweet.extendedEntities.media.length > 0;
   const hasPhoto = tweet.entities && tweet.entities.media && tweet.entities.media.length > 0;
 
@@ -50,39 +51,27 @@ const getMediaInfo = async (data) => {
       }
     })
   }
+
   return mediaInfo;
 }
 
-const getLikes = async (userId) => {
-  const client = await api.getClientFromCookies(config.ct0, config.authToken)
-  const results = []
+const getLikesMedia = async (userId) => {
   const response = await client.getTweetApi().getLikes({ userId: userId }) // userIDからいいね欄を取得
   // console.log(response.data.data.length)
-  response.data.data.filter((e) => !e.promotedMetadata && (!!e.tweet.legacy.entities.media || !!e.tweet.legacy.extendedEntities)) // プロモーションツイートと画像のないツイートを除外
-    .forEach((data) => {
-      getMediaInfo(data)
-        .then((e) => e.forEach((mediaInfo) => {
-          // sqlの中のデータと照合して、存在しなかったら保存する
-          checkSQL(data, mediaInfo)
-            .then((existInSQL) => {
-              if (existInSQL) {
-                results.push(mediaInfo)
-              }
-            })
-        }))
-      // 複数枚画像のあるツイートについて、一枚ずつ処理
-    })
-
-  return results;
+  const LikesMediatweets = response.data.data.filter((e) => !e.promotedMetadata && (!!e.tweet.legacy.entities.media || !!e.tweet.legacy.extendedEntities)) // プロモーションツイートと画像のないツイートを除外
+  return LikesMediatweets;
 }
 
 const checkSQL = async (data, mediaInfo) => {
   //resultがすでにSQliteに載ってるかを確認
   let sql
-  sql = "SELECT COUNT(*) FROM tweet WHERE tweet_id = ? LIMIT 1;"
-  db.get(sql, [data.tweet.restId], (err, row) => { // ツイートIDで検索
-    if (err) return console.log("sql error\n" + err.message);
-    if (row['COUNT(*)'] !== 0) return;//console.log("すでにSQliteにあるデータです");
+  db.serialize(() => {
+    sql = "SELECT COUNT(*) FROM tweet WHERE file_name = ? LIMIT 1;"
+    db.get(sql, [mediaInfo.file_name], (err, row) => { // ツイートIDで検索
+      if (err) return console.log("sql error\n" + err.message);
+      if (row['COUNT(*)'] !== 0) return ;
+    })
+
     //新規のデータであった場合resultをSQliteに格納
     sql = "INSERT INTO tweet (author_screenname,author_name, author_id, tweet_id, media_url, file_name) VALUES(?,?,?,?,?,?);"
     const param = [
@@ -93,7 +82,6 @@ const checkSQL = async (data, mediaInfo) => {
       mediaInfo.url, // 画像のurl
       mediaInfo.file_name, // ファイル名
     ]
-    console.log(param)
     db.run(sql, param, (err) => {
       if (err) return console.log("sql error\n" + err.message);
       return true;
@@ -107,7 +95,6 @@ const downloadPic = async (pic) => {
     if (!response.ok) {
       throw new Error(`Failed to download ${pic.url}, status: ${response.status}`)
     }
-    // const outputPath = pic.media_type === "photo" ? `${savepath}/${pic.file_name}.jpg` : `${savepath}/${pic.file_name}.mp4`
     const outputPath = `${config.savepath}/${pic.file_name}`
     const imageBuffer = Buffer.from(await response.arrayBuffer())
     await writeFile(outputPath, imageBuffer)
@@ -117,21 +104,32 @@ const downloadPic = async (pic) => {
 }
 
 
-const main = async () => {
+const getAllMedia = async () => {
   const allPictures = []
   // ユーザごとに処理を実行し、画像を取得する Process the users and get their pictures
   for await (const user of config["UserList"]) {
-    const userPictures = await getLikes(user.userId)
-    allPictures.push(...userPictures)
-    console.log(`${user.username}の画像を取得しました\n${userPictures.length}枚取得`)
+    const tweets = await getLikesMedia(user.userId); // メディアツイート情報取得
+    for await (const tweet of tweets) { // ツイート個々に対して処理
+      const mediaInfo = await getMediaInfo(tweet);
+      // sqlの中のデータと照合して、存在しなかったら保存する
+      for (const media of mediaInfo) {
+        const existInSQL = await checkSQL(tweet, media)
+        if (existInSQL) {
+          allPictures.push(media)
+        }
+      }
+    }
+    console.log(`${user.username}の画像を取得しました\n${allPictures.length}枚取得`)
   }
   db.close()
-  // console.log(allPictures) // デバック用
+  return allPictures;
+}
+const main = async () => {
+  const allPictures = await getAllMedia()
   // すべての画像をダウンロードする Download all the pictures
-  for await (const pic of allPictures) {
+  for (const pic of allPictures) {
     await downloadPic(pic)
   }
-  console.log(allPictures)
   console.log("おわり")
 }
 
